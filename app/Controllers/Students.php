b61df8f1343e1f2769ecef3e9826e7f4401a30f9
pradeepdsmk <pradeepdsmk@gmail.com>
2021-06-10 13:47:13 -0700

    students list from database

--- app/Controllers/Students.php
+++ app/Controllers/Students.php
@@ -7,11 +7,11 @@ class Students extends BaseController
     public function index()
     {
         $students = $this->db->query(
-            'select s.name, s.course, s.joinedon, s.coursefee, count(fp. 
+            'select s.name, s.course, s.joiningdate 
             from students s 
             left join feepayments fp on s.studentid = fp.studentid 
-            order by s.joinedon 
+            order by s.joiningdate desc  
             limit 0, 25;')->getResult();
-        return view('students/students-list');
+        return view('students/students-list', ['students' => $students]);
     }
 }
--- app/Views/students/students-list.php
+++ app/Views/students/students-list.php
@@ -3,29 +3,39 @@
 
 <?= $this->section('content') ?>
 
-<div class="students-list-wrap">
-    <h1>Students</h1>
+<div class="module students-list-wrap mt-5" data-module="StudentsList">
+    <div class="container">
+        <h3 class="page-title">Students</h3>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Course</th>
-                <th>Joined on</th>
-                <th>Fee status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($students as $student) : ?>
-            <tr>
-                <th><?= $student->name ?></th>
-                <th><?= $student->course ?></th>
-                <th><?= $student->joinedon ?></th>
-                <th><?= $student->feestatus ?></th>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+        <?php if (empty($students)) : ?>
+            <p class="text-center mt-5">No students <a class="btn btn-light ml-3" href="/applicationform">Add</a></p>
+        <?php else : ?>
+
+            <div class="table-responsive">
+
+                <table class="table table-bordered">
+
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Course</td>
+                            <td>Joined on</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($students as $student) : ?>
+                            <tr>
+                                <td><?= $student->name ?></td>
+                                <td><?= $student->course ?></td>
+                                <td><?= date('d/m/Y', strtotime($student->joiningdate)) ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+
+        <?php endif; ?>
+    </div>
 </div>
 
 <?= $this->endSection() ?>
